0df972d2f4a828ed2f7a4cfa72ea7ce7fe131184
fahadjan2 <fahadfawad2006@gmail.com>
2025-12-10 20:14:08 -0800

    Milestone 2

--- code.cpp
+++ code.cpp
@@ -1,23 +1,38 @@
 #include <iostream>
 #include <fstream>
 #include <map>
+#include <vector>
 
 using namespace std;
 
 int main() {
     map <string, int> flights;
     
-    //File Opening
     ifstream file;
     file.open("data.txt");
-    //File Populating
     string line;
-    while (file >> line) {
+    while (file >> line) 
         flights[line]++;
-    }
     file.close();
 
-    for (auto pair : flights) {
+    cout << "All airport traffic counts:" << endl;
+    for (auto pair : flights) 
         cout << pair.first << ": " << pair.second << endl;
+    cout << endl;
+
+    vector<string> airports;
+    int highestTraffic = 0;
+    for (auto pair : flights) {
+        if (pair.second > highestTraffic) {
+            highestTraffic = pair.second;
+            airports.clear();
+            airports.push_back(pair.first);
+        } else if (pair.second == highestTraffic) {
+            airports.push_back(pair.first);
+        }
     }
+    cout << "Busiest airport(s) with count " << highestTraffic << ":" << endl;
+    for (string value : airports) 
+        cout << value << " " << highestTraffic << endl;
+    
 }
