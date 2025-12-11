efc1f2c5271c49b53349a487fa169265b8e2aa81
fahadjan2 <fahadfawad2006@gmail.com>
2025-12-10 20:06:28 -0800

    Milestone 1

--- code.cpp
+++ code.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
+#include <fstream>
+#include <map>
 
 using namespace std;
 
 int main() {
+    map <string, int> flights;
     
+    //File Opening
+    ifstream file;
+    file.open("data.txt");
+    //File Populating
+    string line;
+    while (file >> line) {
+        flights[line]++;
+    }
+    file.close();
+
+    for (auto pair : flights) {
+        cout << pair.first << ": " << pair.second << endl;
+    }
 }
