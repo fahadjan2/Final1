01637c4b81ca2f768955ff3b50a8d967c7fdb3a9
fahadjan2 <fahadfawad2006@gmail.com>
2025-12-10 20:19:05 -0800

    Milestone 3

--- code.cpp
+++ code.cpp
@@ -5,6 +5,8 @@
 
 using namespace std;
 
+void trafficRanges(map <string, int>, int, int);
+
 int main() {
     map <string, int> flights;
     
@@ -34,5 +36,18 @@ int main() {
     cout << "Busiest airport(s) with count " << highestTraffic << ":" << endl;
     for (string value : airports) 
         cout << value << " " << highestTraffic << endl;
-    
+    cout << endl;
+
+    trafficRanges(flights, 5, 8);
+    trafficRanges(flights, 9, 12);
+}
+
+void trafficRanges(map <string, int> flights, int low, int high) {
+    cout << "Airports with traffic in range [" << low << ", " << high << "]:" << endl;
+    for (auto pair : flights) {
+        if (pair.second >= low && pair.second <= high) {
+            cout << pair.first << " " << pair.second << endl;
+        }
+    }
+    cout << endl;
 }
